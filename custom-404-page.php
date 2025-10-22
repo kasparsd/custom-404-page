98a755c64d059697f2b60ddd0e5d1e938957ba69
Kaspars Dambis <hi@kaspars.net>
2025-10-22 17:33:40 +0300

    Better slogan

--- custom-404-page.php
+++ custom-404-page.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom 404 Error Page
  * Plugin URI: https://github.com/kasparsd/custom-404-page
- * Description: Set any page to be used as 404 error page.
+ * Description: Set any page as a 404 error page.
  * Version: 0.2.6
  * Author: Kaspars Dambis
  * Text Domain: custom-404-page
