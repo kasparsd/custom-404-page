ea8ee1dd3824961a071a11ee612913c0f419109e
Kaspars Dambis <hi@kaspars.net>
2025-10-22 17:04:46 +0300

    Nicer header formatting

--- custom-404-page.php
+++ custom-404-page.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: Custom 404 Error Page
-Plugin URI: https://github.com/kasparsd/custom-404-page
-Description: Set any page to be used as 404 error page.
-Version: 0.2.6
-Author: Kaspars Dambis
-Text Domain: custom-404-page
-*/
+/**
+ * Plugin Name: Custom 404 Error Page
+ * Plugin URI: https://github.com/kasparsd/custom-404-page
+ * Description: Set any page to be used as 404 error page.
+ * Version: 0.2.6
+ * Author: Kaspars Dambis
+ * Text Domain: custom-404-page
+ */
 
 if ( ! function_exists( 'add_action' ) ) {
 	return;
